fff3bc19887b083e04de4d5f31487d3d0921148c
William C. Johnson <wcjohnson@oigroup.net>
2017-05-13 23:17:59 -0400

    Fix cli parser config to match tests

--- bin/babylon-lightscript.js
+++ bin/babylon-lightscript.js
@@ -11,6 +11,6 @@ if (!filename) {
 }
 
 var file = fs.readFileSync(filename, "utf8");
-var ast  = babylon.parse(file, { plugins: ["lightscript", "flow"] });
+var ast  = babylon.parse(file, { plugins: ["lightscript", "jsx", "flow"] });
 
 console.log(JSON.stringify(ast, null, "  "));
