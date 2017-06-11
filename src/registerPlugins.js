cee1f8f4c6a66ffc3f310f11318d5d4e21570646
William C. Johnson <wcjohnson@oigroup.net>
2017-06-11 14:20:52 -0400

    Register and expose available parser plugins via api

---
+++ src/registerPlugins.js
@@ -0,0 +1,26 @@
+import lightscriptPlugin from "./plugins/lightscript";
+import estreePlugin from "./plugins/estree";
+import flowPlugin from "./plugins/flow";
+import jsxPlugin from "./plugins/jsx";
+
+function noncePlugin() {}
+
+export default function registerPlugins(plugins) {
+  plugins.lightscript = lightscriptPlugin;
+  plugins.estree = estreePlugin;
+  plugins.flow = flowPlugin;
+  plugins.jsx = jsxPlugin;
+
+  // Plugins with string tags only
+  ([
+    "doExpressions",
+    "objectRestSpread",
+    "decorators",
+    "classProperties",
+    "exportExtensions",
+    "asyncGenerators",
+    "functionBind",
+    "functionSent",
+    "dynamicImport",
+  ]).forEach((pluginName) => plugins[pluginName] = noncePlugin);
+}
