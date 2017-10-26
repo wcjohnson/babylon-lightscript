2a7dfbcde8fdfd102b90057ac3a3d8afc94a103b
William C. Johnson <wcjohnson@oigroup.net>
2017-10-25 22:20:44 -0400

    Catchless and paramless try

--- src/plugins/enhancedTry.js
+++ src/plugins/enhancedTry.js
@@ -10,7 +10,7 @@ export default function(parser) {
   pp.parseTry = function(node, isExpression) {
     this.next();
 
-    let indentLevel, isBrace;
+    let indentLevel, isBrace = true;
     if (this.hasPlugin("lightscript")) {
       indentLevel = this.state.indentLevel;
       isBrace = this.match(tt.braceL);
@@ -31,7 +31,7 @@ export default function(parser) {
       }
     }
 
-    if (!node.handler && !node.finalizer) {
+    if (isBrace && !node.handler && !node.finalizer) {
       this.raise(node.start, "Missing catch or finally clause");
     }
 
@@ -52,7 +52,7 @@ export default function(parser) {
   pp.parseCatchBlock = function(node, tryIndentLevel, isBrace) {
     const shouldParseCatch = this.match(tt._catch) && (
       !this.hasPlugin("lightscript") ||
-      tryIndentLevel === this.state.indentLevel ||
+      tryIndentLevel <= this.state.indentLevel ||
       (isBrace && !this.matchBlockState("try", this.state.indentLevel))
     );
 
@@ -60,24 +60,17 @@ export default function(parser) {
       const clause = this.startNode();
       this.next();
 
-      if (this.hasPlugin("lightscript")) {
-        if (this.eat(tt.parenL)) {
-          this.addExtra(clause, "hasParens", true);
-        }
-      } else {
-        this.expect(tt.parenL);
+      clause.param = null;
+      if (!this.match(tt.colon) && !this.match(tt.braceL)) {
+        this.parseCatchClauseParam(clause);
       }
-      clause.param = this.parseBindingAtom();
-      this.checkLVal(clause.param, true, Object.create(null), "catch clause");
+
       if (this.hasPlugin("lightscript")) {
         this.expectParenFreeBlockStart(clause);
         if (!this.match(tt.colon) && !this.match(tt.braceL)) {
           this.unexpected(null, "Expected a block.");
         }
-      } else {
-        this.expect(tt.parenR);
       }
-
       // Detect block vs cases
       const next2 = this.tokenLookahead(2);
       if (next2[0] === tt.bitwiseOR || next2[2] === tt.bitwiseOR) {
@@ -90,10 +83,25 @@ export default function(parser) {
     }
   };
 
+  pp.parseCatchClauseParam = function(clause) {
+    if (this.hasPlugin("lightscript")) {
+      if (this.eat(tt.parenL)) {
+        this.addExtra(clause, "hasParens", true);
+      }
+    } else {
+      this.expect(tt.parenL);
+    }
+    clause.param = this.parseBindingAtom();
+    this.checkLVal(clause.param, true, Object.create(null), "catch clause");
+    if (!this.hasPlugin("lightscript")) {
+      this.expect(tt.parenR);
+    }
+  };
+
   pp.parseFinallyBlock = function(node, tryIndentLevel, isBrace) {
     if (this.hasPlugin("lightscript")) {
       const shouldParseFinally = this.match(tt._finally) && (
-        tryIndentLevel === this.state.indentLevel ||
+        tryIndentLevel <= this.state.indentLevel ||
         (isBrace && !this.matchBlockState("try", this.state.indentLevel))
       );
       if (shouldParseFinally) {
